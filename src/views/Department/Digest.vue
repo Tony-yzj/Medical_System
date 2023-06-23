5a4fa3f6372381adc98ef0c87e8627247ea3592f
Tony-yzj <1634644151@qq.com>
2023-06-23 20:51:15 +0800

    add better book

--- src/views/Department/Digest.vue
+++ src/views/Department/Digest.vue
@@ -21,8 +21,9 @@
               :key="timeIndex"
               :class="{
                 'time-slot': true,
-                'available': available,
-                'booked': !available,
+                'available': (getBusyStatus(dayIndex, timeIndex) == 0),
+                'half-booked':(getBusyStatus(dayIndex, timeIndex) == 2),
+                'booked': (getBusyStatus(dayIndex, timeIndex) == 1),
                 'gray': !isAvailable(dayIndex, timeIndex)
               }"
               @click="book(dayIndex, timeIndex)"
@@ -34,12 +35,12 @@
       </table>
     </div>
 
-    <div v-if="selectedDoctor" class="doctor-intro">
+    <!-- <div v-if="selectedDoctor" class="doctor-intro">
       <h3>医生简介</h3>
       <p class="doctor-name">{{ selectedDoctor.name }}</p>
       <p class="doctor-title">{{ selectedDoctor.title }}</p>
       <p>{{ selectedDoctor.introduction }}</p>
-    </div>
+    </div> -->
     <div class="doctor">
         <h3>本周门诊医生介绍</h3>
         <ul v-for="(item, index) in doctors" :key="index">
@@ -90,12 +91,40 @@ export default {
         {
           name: '张三',
           title: '主任医师',
-          introduction: '张三是消化内科的主任医师，拥有丰富的临床经验。'
+          introduction: '张三是消化内科的主任医师，拥有丰富的临床经验。',
+          schedule:[
+          [false, false, true, false, true, true, true],
+          [true, true, true, false, false, true, true],
+          [true, false, true, false, true, false, true],
+          [true, true, false, false, false, true, true],
+          [true, false, true, true, false, true, false]
+        ],
+        book:[
+          [false, false, false, false, false, false, false],
+          [false, false, true, false, false, false, true],
+          [false, false, false, false, false, false, false],
+          [false, false, false, false, false, false, false],
+          [false, false, false, false, false, true, false]
+        ]
         },
         {
           name: '李四',
           title: '副主任医师',
-          introduction: '李四是消化内科的副主任医师，擅长胃肠道疾病的治疗。'
+          introduction: '李四是消化内科的副主任医师，擅长胃肠道疾病的治疗。',
+          schedule:[
+          [true, false, true, false, true, true, true],
+          [true, true, true, true, false, true, true],
+          [false, false, true, true, true, false, true],
+          [true, true, false, false, false, true, true],
+          [true, true, true, true, true, true, false]
+        ],
+        book:[
+          [false, false, false, false, false, false, false],
+          [false, false, false, false, false, false, true],
+          [false, false, false, false, false, false, false],
+          [false, false, false, false, false, false, false],
+          [false, false, false, false, false, false, false]
+        ]
         }
       ],
       selectedDay: null,
@@ -106,15 +135,32 @@ export default {
   },
   methods: {
     getTimeSlotsAvailability(dayIndex) {
+      this.refreshSchedule(dayIndex);
       return this.schedule[dayIndex];
+    },
+    refreshSchedule(dayIndex){
+      for(var index = 0; index < 7; index=index+1){
+        this.schedule[dayIndex][index] = false;
+        this.doctors.forEach(doctor => {
+          if (this.isdoctorAvailable(doctor, dayIndex, index)) {
+            this.schedule[dayIndex][index] = true;
+          }
+        });
+      }
     },
     isAvailable(dayIndex, timeIndex) {
       return this.schedule[dayIndex][timeIndex];
     },
+    isdoctorAvailable(doctor, dayIndex, timeIndex) {
+      return doctor.schedule[dayIndex][timeIndex];
+    },
+    isdoctorBusy(doctor, dayIndex, timeIndex) {
+      return doctor.book[dayIndex][timeIndex];
+    },
     getDoctorsAvailability(dayIndex, timeIndex) {
       const availableDoctors = [];
       this.doctors.forEach(doctor => {
-        if (this.isAvailable(dayIndex, timeIndex)) {
+        if (this.isdoctorAvailable(doctor, dayIndex, timeIndex)) {
           availableDoctors.push(doctor.name);
         }
       });
@@ -123,14 +169,43 @@ export default {
     getAvailableDoctors(dayIndex, timeIndex) {
       const availableDoctors = [];
       this.doctors.forEach(doctor => {
-        if (this.isAvailable(dayIndex, timeIndex)) {
+        if (this.isdoctorAvailable(doctor, dayIndex, timeIndex)) {
           availableDoctors.push(doctor);
         }
       });
       return availableDoctors;
     },
+    getFreeDoctor(dayIndex, timeIndex) {
+      const availableDoctors = [];
+      this.doctors.forEach(doctor => {
+        if (this.isdoctorAvailable(doctor, dayIndex, timeIndex) && !this.isdoctorBusy(doctor, dayIndex, timeIndex)) {
+          availableDoctors.push(doctor);
+        }
+      });
+      return availableDoctors;
+    },
+    getBusyStatus(dayIndex, timeIndex) {
+      const availableDoctors = this.getAvailableDoctors(dayIndex, timeIndex);
+      let cnt = 0;
+      let free = 0;
+      availableDoctors.forEach(doctor => {
+        if (!doctor.book[dayIndex][timeIndex]) {
+          free++;
+        }
+        cnt++;
+      });
+      if (cnt === 0) {
+        return -1; // 没有可用的医生
+      } else if (free === cnt) {
+        return 0; // 所有医生都可用
+      } else if (free === 0) {
+        return 1; // 所有医生都已预约
+      } else {
+        return 2; // 部分医生可用
+      }
+    },
     book(dayIndex, timeIndex) {
-      if (this.isAvailable(dayIndex, timeIndex)) {
+      if (this.getBusyStatus(dayIndex, timeIndex) == 0 || this.getBusyStatus(dayIndex, timeIndex) == 2) {
         this.selectedDay = dayIndex;
         this.selectedTimeSlot = timeIndex;
         this.dialogVisible = true;
@@ -143,9 +218,43 @@ export default {
       this.dialogVisible = false;
     },
     confirmBooking() {
-      // 在这里处理预约逻辑
-      console.log('预约成功！');
-      this.dialogVisible = false;
+      if (this.selectedDoctor) {
+        const selectedDoctor = this.selectedDoctor;
+        const selectedDay = this.selectedDay;
+        const selectedTimeSlot = this.selectedTimeSlot;
+        var flag = 1;
+        // 修改预约时间
+        // selectedDoctor.book[selectedDay][selectedTimeSlot] = true;
+        const availableDoctors = this.getAvailableDoctors(selectedDay, selectedTimeSlot);
+        availableDoctors.forEach(doctor => {
+          if (doctor.name == selectedDoctor) {
+            if(doctor.book[selectedDay][selectedTimeSlot] == true)
+            {
+              flag = 0;
+            }
+            doctor.book[selectedDay][selectedTimeSlot] = true;
+          }
+        });
+
+        // 清空选中的医生
+        this.selectedDoctor = null;
+
+        if(flag)
+        {
+          console.log('预约成功！');
+          this.$message.success('预约成功');
+          this.dialogVisible = false;
+        }
+        else
+        {
+          console.log('预约失败');
+          this.$message.error('预约失败，该医生已被预约');
+          this.dialogVisible = false;
+        }
+      } else {
+        console.log('请选择医生');
+        this.$message.warning('请选择医生');
+      }
     }
   }
 };
@@ -186,6 +295,11 @@ th {
   background-color: red;
   color: white;
   cursor: not-allowed;
+}
+
+.half-booked {
+  background-color: rgb(246, 165, 14);
+  color: white;
 }
 
 .gray {
